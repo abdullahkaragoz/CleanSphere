c33ad30948981139eef793981f12ab1f8cc40693
Abdullah Karagöz <abdullah.karagoz@outlook.com>
2024-11-28 01:28:52 +0300

    GenericRepsitory

---
+++ Repositories/GenericRepsitory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repositories
+{
+    public class GenericRepsitory<T>(AppDbContext context) : IGenericRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public IQueryable<T> GetAll() => _dbSet.AsQueryable().AsNoTracking();
+        
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+        
+        public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public async ValueTask<T?> GetByIdAsync(T entity) => await _dbSet.FindAsync(entity);
+        
+        public void Update(T entity) => _dbSet.Update(entity);
+        
+        public void Delete(T entity) => _dbSet.Remove(entity);
+
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+
+namespace Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    IQueryable<T> GetAll();
+    IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+    ValueTask<T?> GetByIdAsync(T entity);
+    ValueTask AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
